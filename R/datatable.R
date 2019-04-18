05e844554294e53717bdeaa68d9d90a7c0f579ff
Emir Turkes <eturkes@bu.edu>
2019-04-18 16:03:14 -0400

    Replace single quotes with double quotes

--- R/datatable.R
+++ R/datatable.R
@@ -2,10 +2,10 @@
 
 datatable <- function(dt) {
     DT::datatable(
-        dt, extensions = 'Buttons', options = list(
-            dom = 'Bfrtip', buttons = list(
-                'copy', 'print', list(
-                    extend = 'collection', buttons = c('csv', 'excel', 'pdf'), text = 'Download'
+        dt, extensions = "Buttons", options = list(
+            dom = "Bfrtip", buttons = list(
+                "copy", "print", list(
+                    extend = "collection", buttons = c("csv", "excel", "pdf"), text = "Download"
                 )
             )
         )
