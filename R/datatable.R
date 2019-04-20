731270210abd46d79f8886535f105076646a660d
Emir Turkes <eturkes@bu.edu>
2019-04-19 20:00:16 -0400

    Add length changing input control to datatable

--- R/datatable.R
+++ R/datatable.R
@@ -3,7 +3,7 @@
 datatable <- function(dt) {
     DT::datatable(
         dt, extensions = "Buttons", options = list(
-            dom = "Bfrtip", buttons = list(
+            dom = "Blfrtip", buttons = list(
                 "copy", "print", list(
                     extend = "collection", buttons = c("csv", "excel", "pdf"), text = "Download"
                 )
