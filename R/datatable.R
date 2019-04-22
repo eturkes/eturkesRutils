5bc02e73194cb698641ed9fd03cac9cff78493a3
Emir Turkes <eturkes@bu.edu>
2019-04-22 15:06:20 -0400

    Add conversion to exponential to datatable

--- R/datatable.R
+++ R/datatable.R
@@ -1,13 +1,29 @@
-# A custom datatable with additional buttons
+# A custom datatable with additional features and formatting.
 
 datatable <- function(dt) {
     DT::datatable(
-        dt, extensions = "Buttons", options = list(
-            dom = "Blfrtip", buttons = list(
+        dt,
+        extensions = "Buttons",
+        options = list(
+            dom = "Blfrtip",
+            buttons = list(
                 "copy", "print", list(
-                    extend = "collection", buttons = c("csv", "excel", "pdf"), text = "Download"
+                    extend = "collection", buttons = c(
+                        "csv", "excel", "pdf"
+                    ),
+                    text = "Download"
                 )
+            ),
+            rowCallback = DT::JS(
+                "function(row, data) {",
+                "for (i = 1; i < data.length; i++) {",
+                "if (data[i]>1000 | data[i]<1){",
+                "$('td:eq('+i+')', row).html(data[i].toExponential(2));",
+                "}",
+                "}",
+                "}"
             )
         )
-    )
+    ) %>%
+        DT::formatRound(2)
 }
